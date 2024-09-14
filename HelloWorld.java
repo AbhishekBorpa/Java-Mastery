e995b36643fc8c959756c1fb908899d799d7fff5
Abhishek <abhishekborpa@gmail.com>
2024-09-14 16:05:32 +0530

    hehe

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,15 @@ public class HelloWorld{
       System.out.println("Hello World");
       int a = 10;
       System.out.println(a);
+      double b = 20.98;
+      char c  = 'A';
+      boolean isTrue  = true;
+      System.out.println("b"+b);
+      System.out.println("c"+c);
+      System.out.println("IsTrue"+isTrue);
+
+
+
+
     }
 }
